f0a3baeaf310c721d78b1ddd0500523acb8aad96
draskeer <tom.alarcon@draskeer.fr>
2024-12-16 14:37:26 +0100

    Created the core of the api.

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,23 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const userRoutes = require("./routes/users");
+const classRoutes = require("./routes/class");
+const studentRoutes = require("./routes/students");
 
 const app = express();
+const port = 3434;
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/', userRoutes);
+app.use('/api/class', classRoutes);
+app.use('/api/students', studentRoutes);
+
+
+mongoose.connect('mongodb+srv://ScolarVinci:QJTBPX9FCINs0J4c@miseensituationdev.8gghw.mongodb.net/?retryWrites=true&w=majority&appName=MiseEnSituationDev')
+    .then(() => {
+        console.log("Connexion à MongoDB réussie");
+        app.listen(port, () => console.log("Serveur démarré sur le port:", port));
+    })
+    .catch((error) => console.error("Erreur de connexion à MongoDB :", error));
