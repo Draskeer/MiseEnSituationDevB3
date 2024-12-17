c71f348aafb74aeee175612a3d2944af7336b217
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2024-12-17 11:02:20 +0100

    get and post on api

--- Backend/routes/students.js
+++ Backend/routes/students.js
@@ -3,11 +3,26 @@ const router = express.Router();
 const Student = require("../models/Student");
 router.get("/", async (req, res) => {
   try {
-    const students = Student.find({});
+    const students = await Student.find({});
     res.json(students);
   } catch (error) {
     console.error(error);
   }
 });
-router.get();
+
+router.post("/", async (req, res) => {
+  try {
+    const newStudents = req.body;
+    res.json(await Student.insertMany(newStudents));
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    res.json(student);
+  } catch (error) {}
+});
 module.exports = router;
